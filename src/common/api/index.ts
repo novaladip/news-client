19567911fc273414b3db23f1a61de2e3872301fc
A. Noval Adip <a.novaladip@yahoo.com>
2019-11-13 04:20:28 +0700

    [Common/api] add api instance, handleAxiosError & setDefaultAuthJwt

---
+++ src/common/api/index.ts
@@ -0,0 +1,19 @@
+import axios, { AxiosError } from "axios";
+
+export const api = axios.create({
+  baseURL: "http://localhost:8000"
+});
+
+export function handleAxiosError(error: AxiosError): any {
+  if (error.response) {
+    return error.response.data;
+  } else {
+    return {
+      message: "Failed to reach the server, please try again."
+    };
+  }
+}
+
+export function setDefaultAuthJwt(jwt: string): void {
+  api.defaults.headers["Authorization"] = jwt;
+}
