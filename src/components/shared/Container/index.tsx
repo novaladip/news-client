edf46e3779d0f5fc3d4109b06e2a6c655e7f1e9d
A. Noval Adip <a.novaladip@yahoo.com>
2019-11-13 17:44:12 +0700

    [components/shared] update container props

--- src/components/shared/Container/index.tsx
+++ src/components/shared/Container/index.tsx
@@ -3,13 +3,16 @@ import { Box } from "@chakra-ui/core";
 
 interface Props {
   children: React.ReactNode;
+  height?: number | string;
 }
 
 export function Container(props: Props) {
   return (
     <Box
+      h={props.height}
       d="flex"
       w="100%"
+      alignItems="center"
       justifyContent="center"
       alignSelf="center"
       flexDirection="column"
