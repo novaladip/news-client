074b92e85956823be9a2bf730b385e63926a7b8f
A. Noval Adip <a.novaladip@yahoo.com>
2019-11-14 21:21:49 +0700

    [store] update store

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,14 +2,18 @@ import { createStore } from "easy-peasy";
 
 import { AuthenticationModel } from "./authentication/model";
 import { authenticationModel } from "./authentication";
+import { NewsModel } from "./news/model";
+import { newsModel } from "./news";
 export * from "./hooks";
 
 export interface StoreModel {
   authentication: AuthenticationModel;
+  news: NewsModel;
 }
 
 const storeModel: StoreModel = {
-  authentication: authenticationModel
+  authentication: authenticationModel,
+  news: newsModel
 };
 
 const store = createStore(storeModel);
